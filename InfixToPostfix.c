e6ad68d220a99dbbcfc9a6f439aedcdce7cf5431
Praneeth2k <47350983+Praneeth2k@users.noreply.github.com>
2019-08-21 12:52:21 +0530

    Update InfixToPostfix.c

--- InfixToPostfix.c
+++ InfixToPostfix.c
@@ -1,37 +1,41 @@
 #include<stdio.h>
 #include<ctype.h>
-#include<string.h>
 void push(char stack[30],int *t,char ele);
 void pop(char s[30],int *t);
 int precedence(char o);
-void postfix(char infix[30],char pf[30],int *t1);
+void postfix(char infix[30]);
 int main()
 {
-  int t=-1,t1=-1;
-  char s[30]={'(','A','+','(','B','-','c',')','*','D',')'};
-  char pstfix[30];
+  int t=-1,i=-1;
+  char s[] = "a+b*(c^d-e)^(f+g*h)-i";
+  s[21]='\n';
+
   
-  postfix(s,pstfix,&t1);
+ 
+  
+  
+  postfix(s);
   return 0;
 }
-void postfix(char infix[30],char pf[30],int *t1)
+void postfix(char infix[30])
 {
- int i,tops=-1;
+ int i=0,tops=-1;
  char stack[30];
- for(i=0;i<11;i++)
+ while(infix[i]!='\n')
   {
    if(isalpha(infix[i]))
-     {printf("%c",infix[i]);
-     push(pf,t1,infix[i]);}
-   else if((infix[i]=='+')||(infix[i]=='-')||(infix[i]=='*')||(infix[i]=='/'))
+     printf("%c",infix[i]);
+     
+   else if((infix[i]=='+')||(infix[i]=='-')||(infix[i]=='*')||(infix[i]=='/')||(infix[i]=='^'))
     {
      if((precedence(infix[i])>precedence(stack[tops]))||(stack[tops]=='(')||(tops==-1))
 	push(stack,&tops,infix[i]);
      else
        {
        while(precedence(stack[tops])>=precedence(infix[i]))
-	 pop(stack,&tops);
-	 push(stack,&tops,infix[i]);
+	    {printf("%c",stack[tops]);
+	     pop(stack,&tops);}
+	    push(stack,&tops,infix[i]);
        }
     }
    if(infix[i]=='(')
@@ -41,35 +45,40 @@ void postfix(char infix[30],char pf[30],int *t1)
       while (stack[tops]!='(')
       {
        printf("%c",stack[tops]);
-       pop(stack,*tops);
+       
+       pop(stack,&tops);
       }
-      pop(stack,*tops);
+      pop(stack,&tops);
       }
-   }
+  
+      i++;
+  }
     for(i=tops;i>=0;i--)
 	  printf("%c",stack[i]);
-   getch();
-   }
+    
+}
+   
 
-void push(char stack[30],int *t,int ele)
-{
-   stack[++*t]=e;
+void push(char stack[30],int *t,char ele)
+{  
+    *t=*t+1; 
+   stack[*t]=ele;
    return;
 }
-void pop(int s[30],*t)
+void pop(char s[30],int *t)
 {
    *t=*t-1;
    return;
 }
 int precedence(char c)
 {
- if((c=='^')
-   return 3;
+ if(c=='^')
+   {return 3;}
  else if((c=='*')||(c=='/'))
-   return 2;
+   {return 2;}
  else if((c=='+')||(c=='-'))
-   return 1;
+   {return 1;}
  else
-   return 0;
+   {return 0;}
 }
 
